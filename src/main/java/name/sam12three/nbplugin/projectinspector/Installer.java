46b53945a378ba6b23eb4d84dcf9fc1ea3297357
Sam Sharaf <sam.sharaf@outlook.com>
2025-11-10 19:29:52 -0800

    netbeans.installation=/Users/samsharaf/platform-release28/netbeans/nbbuild/netbeans

---
+++ src/main/java/name/sam12three/nbplugin/projectinspector/Installer.java
@@ -0,0 +1,16 @@
+package name.sam12three.nbplugin.projectinspector;
+
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall {
+
+    @Override
+    public void restored() {
+        // Called when the module is loaded
+    }
+    
+    @Override
+    public void uninstalled() {
+        // Called when the module is uninstalled
+    }
+}
